3d125c993de573c279a9e990824a4c85a3257407
maple3142 <kirby741852963@gmail.com>
2018-06-29 19:48:41 +0800

    update start confirm

--- scripts/bahamut-star-confirm.user.js
+++ scripts/bahamut-star-confirm.user.js
@@ -1,20 +1,26 @@
 // ==UserScript==
 // @name         巴哈姆特收藏確認
 // @namespace    https://blog.maple3142.net/
-// @version      0.1
+// @version      0.2
 // @description  在收藏前彈出一個對話框詢問是否要收藏
 // @author       maple3142
 // @match        https://forum.gamer.com.tw/C.php?*
+// @match        https://home.gamer.com.tw/*
 // @grant        none
 // ==/UserScript==
 
 ;(function() {
 	'use strict'
-
-	const bak = FORUM_homeBookmark
-	FORUM_homeBookmark = (...args) => {
-		if (confirm('確認要收藏嗎?')) {
-			bak.apply(null, args)
-		}
-	}
+	if (typeof FORUM_homeBookmark !== 'undefined')
+		FORUM_homeBookmark = (fn => (...args) => {
+			if (confirm('確認要收藏嗎?')) {
+				fn(...args)
+			}
+		})(FORUM_homeBookmark)
+	if (typeof homeBookmarkNew !== 'undefined')
+		homeBookmarkNew = (fn => (...args) => {
+			if (confirm('確認要收藏嗎?')) {
+				fn(...args)
+			}
+		})(homeBookmarkNew)
 })()
