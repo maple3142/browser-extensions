ec8f7dbb92b378c15bb837e5f62e4c777f2bd63e
maple3142 <kirby741852963@gmail.com>
2018-07-02 10:51:46 +0800

    add Steam totally disable autoplay

---
+++ scripts/steam-totally-disable-autoplay.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Steam totally disable autoplay
+// @namespace    https://blog.maple3142.net/
+// @version      0.1
+// @description  Totally disable steam autoplay
+// @author       maple3142
+// @match        https://store.steampowered.com/app/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+;(function() {
+	'use strict'
+	const parseCookie = cookie =>
+		Object.assign(
+			...cookie
+				.split(/[;\s]/)
+				.filter(x => x)
+				.map(t => t.split('='))
+				.map(([k, v]) => ({ [k]: decodeURIComponent(v) }))
+		)
+	const serializeCookie = c => Object.keys(c).map(x => `${x}=${encodeURIComponent(c[x])};path=/;`)
+	const setCookie = cookieobj => serializeCookie(cookieobj).forEach(x => (document.cookie = x))
+	const cookie = parseCookie(document.cookie)
+	cookie.bGameHighlightAutoplayDisabled = true
+	setCookie(cookie)
+})()
