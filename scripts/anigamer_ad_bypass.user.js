8851cba343e3c6039f881140c3d56cc3bbcb8099
maple3142 <kirby741852963@gmail.com>
2018-07-09 14:56:48 +0800

    agab: update

--- scripts/anigamer_ad_bypass.user.js
+++ scripts/anigamer_ad_bypass.user.js
@@ -8,19 +8,20 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-;(async () => {
+;(() => {
 	'use strict'
-	const $ = s => document.querySelector(s)
-	const $$ = s => [...document.querySelectorAll(s)]
-	let hasFailed = false
-	await fetch(`/ajax/videoCastcishu.php?sn=${animefun.videoSn}&s=${getAd()[0]}`, {
-		credentials: 'same-origin'
-	}).then(r => r.text())
-	await fetch(`/ajax/videoCastcishu.php?sn=${animefun.videoSn}&s=${getAd()[0]}&ad=end`, {
-		credentials: 'same-origin'
-	}).then(r => r.text())
-	const { src } = await fetch(`/ajax/m3u8.php?sn=${animefun.videoSn}&device=${animefun.getdeviceid()}`, {
-		credentials: 'same-origin'
-	}).then(r => r.json())
-	if (src) console.log('success: %s', src)
+	const sn = animefun.videoSn
+	const device = animefun.getdeviceid()
+	const xhr = (url, o) => fetch(url, { ...o, credentials: 'same-origin' })
+	const qs = o =>
+		Object.keys(o)
+			.map(k => `${k}=${encodeURIComponent(o[k])}`)
+			.join('&')
+	const obj = { sn, s: getAd()[0] }
+	xhr(`/ajax/videoCastcishu.php?` + qs(obj))
+		.then(() => xhr(`/ajax/videoCastcishu.php?` + qs({ ...obj, ad: 'end' })))
+		.then(() => xhr(`/ajax/m3u8.php?` + qs({ sn, device })).then(r => r.json()))
+		.then(({ src }) => {
+			if (src) console.log('success: %s', src)
+		})
 })()
