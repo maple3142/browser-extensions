912a5d6537f58bd6b32b98b02ec8fc1b4a5519a4
maple3142 <kirby741852963@gmail.com>
2018-07-09 14:48:30 +0800

    agab: simplify

--- scripts/anigamer_ad_bypass.user.js
+++ scripts/anigamer_ad_bypass.user.js
@@ -8,39 +8,19 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-;(() => {
+;(async () => {
 	'use strict'
 	const $ = s => document.querySelector(s)
 	const $$ = s => [...document.querySelectorAll(s)]
 	let hasFailed = false
-	async function test() {
-		if ($('#adult')) {
-			$('#adult').click()
-		}
-		await fetch(
-			`/ajax/token.php?adID=${getAd()[0]}&sn=${animefun.videoSn}&device=${animefun.getdeviceid()}&hash=${
-				animefun.uuid
-			}`,
-			{ credentials: 'same-origin' }
-		)
-		await fetch(`/ajax/videoCastcishu.php?sn=${animefun.videoSn}&s=${getAd()[0]}&ad=end`, {
-			credentials: 'same-origin'
-		}).then(r => r.text())
-		const { src } = await fetch(`/ajax/m3u8.php?sn=${animefun.videoSn}&device=${animefun.getdeviceid()}`, {
-			credentials: 'same-origin'
-		}).then(r => r.json())
-		if (src) {
-			if (hasFailed) location.reload()
-		} else {
-			console.log('failed')
-			hasFailed = true
-			setTimeout(test, 100)
-		}
-	}
-	test()
-
-	console.error = (fn => (...args) => {
-		if (args[0].includes('AD ERROR: VAST Error')) location.reload()
-		fn(...args)
-	})(console.error)
+	await fetch(`/ajax/videoCastcishu.php?sn=${animefun.videoSn}&s=${getAd()[0]}`, {
+		credentials: 'same-origin'
+	}).then(r => r.text())
+	await fetch(`/ajax/videoCastcishu.php?sn=${animefun.videoSn}&s=${getAd()[0]}&ad=end`, {
+		credentials: 'same-origin'
+	}).then(r => r.text())
+	const { src } = await fetch(`/ajax/m3u8.php?sn=${animefun.videoSn}&device=${animefun.getdeviceid()}`, {
+		credentials: 'same-origin'
+	}).then(r => r.json())
+	if (src) console.log('success: %s', src)
 })()
