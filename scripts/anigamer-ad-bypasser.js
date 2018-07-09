9cea7cababa7de3758bb521bbd65f39869d9d8ce
maple3142 <kirby741852963@gmail.com>
2018-07-09 16:42:29 +0800

    agab: rename & refactor

--- scripts/anigamer-ad-bypasser.js
+++ scripts/anigamer-ad-bypasser.js
@@ -1,26 +1,28 @@
 // ==UserScript==
-// @name         anigamer_ad_bypass
+// @name         Anigamer ad bypasser
 // @namespace    https://blog.maple3142.net/
 // @version      0.1
-// @description  bypass anigamer ad
+// @description  Bypass ani.gamer.com.tw ads
 // @author       maple3142
 // @match        https://ani.gamer.com.tw/animeVideo.php?sn=*
-// @grant        unsafeWindow
+// @grant        none
 // ==/UserScript==
 
 ;(function() {
 	'use strict'
 	const sn = animefun.videoSn
 	const device = animefun.getdeviceid()
-	const xhr = (url, o) => fetch(url, { ...o, credentials: 'same-origin' })
 	const qs = o =>
 		Object.keys(o)
 			.map(k => `${k}=${encodeURIComponent(o[k])}`)
 			.join('&')
+	const get = url => o => fetch(url + '?' + qs(o), { credentials: 'same-origin' })
+	const getVC = get('/ajax/videoCastcishu.php')
+	const getM3u8 = get('/ajax/m3u8.php')
 	const obj = { sn, s: getAd()[0] }
-	xhr(`/ajax/videoCastcishu.php?` + qs(obj))
-		.then(() => xhr(`/ajax/videoCastcishu.php?` + qs({ ...obj, ad: 'end' })))
-		.then(() => xhr(`/ajax/m3u8.php?` + qs({ sn, device })).then(r => r.json()))
+	getVC(obj)
+		.then(() => getVC({ ...obj, ad: 'end' }))
+		.then(() => getM3u8({ sn, device }).then(r => r.json()))
 		.then(({ src }) => {
 			if (src) console.log('success: %s', src)
 		})
