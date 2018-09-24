06b4f8c149158aee91a6320c6165153cdc7f1fea
maple3142 <kirby741852963@gmail.com>
2018-09-24 10:55:08 +0800

    bhqz: performance update

--- scripts/bahamut-quiz-answer.user.js
+++ scripts/bahamut-quiz-answer.user.js
@@ -1,27 +1,26 @@
 // ==UserScript==
 // @name         巴哈姆特動漫電玩通解答顯示小工具
 // @namespace    https://blog.maple3142.net/
-// @version      0.5
+// @version      0.6
 // @description  在巴哈姆特哈拉區右側的動漫電玩通顯示答案
 // @author       maple3142
+// @require      https://unpkg.com/xfetch-js@0.1.6/xfetch.min.js
 // @match        https://forum.gamer.com.tw/B.php*
-// @compatible   firefox >=52
-// @compatible   chrome >=55
-// @license      MIT
 // ==/UserScript==
 
 ;(function() {
 	'use strict'
 
+	const APIURL = 'https://script.google.com/macros/s/AKfycbyDOc-c9K4PNYzm3S9qGy4nRnGcHDjDwzE_DV_xbNctKs42EO8/exec'
 	const $ansbox = jQuery('.BH-rbox.BH-qabox1')
 
-	const getCSRF = () => jQuery.ajax({ url: '/ajax/getCSRFToken.php', cache: false })
+	const getCSRF = () => xf.get('/ajax/getCSRFToken.php').text()
 	const answer = sn => o =>
 		getCSRF().then(token =>
-			jQuery.get('/ajax/quiz_answer.php', { sn, o, token }).then(html => html.includes('答對了'))
+			xf.get('/ajax/quiz_answer.php', { qs: { sn, o, token } }).text(html => html.includes('答對了'))
 		)
-	const getAnswerOf = sn => Promise.all([1, 2, 3, 4].map(answer(sn))).then(r => r.indexOf(true) + 1)
-
+	const tryAnswer = sn => Promise.all([1, 2, 3, 4].map(answer(sn))).then(r => r.indexOf(true) + 1)
+	const getAnswer = sn => xf.get(APIURL, { qs: { sn } }).json()
 	function getQuestion($ansbox) {
 		let anss = $ansbox
 			.find('li')
@@ -44,19 +43,18 @@
 		})
 	}
 
-	const APIURL = 'https://script.google.com/macros/s/AKfycbyDOc-c9K4PNYzm3S9qGy4nRnGcHDjDwzE_DV_xbNctKs42EO8/exec'
 	function dbQuery(q) {
-		return Promise.resolve(jQuery.get(APIURL)).then(data => {
-			let p
-			if (q.sn in data) p = data[q.sn]
-			else
-				p = getAnswerOf(q.sn).then(ans => {
-					q.answer = ans
-					console.log('Submit new question to database', q)
-					jQuery.post(APIURL, { data: q.toString() })
-					return ans
-				})
-			return p
+		return getAnswer(q.sn).then(data => {
+			console.log(q.sn, data)
+			if (data !== null) {
+				return data.answer
+			}
+			return tryAnswer(q.sn).then(ans => {
+				q.answer = ans
+				console.log('Submit new question to database', q)
+				xf.post(APIURL, { form: { data: q.toString() } }).json(console.log)
+				return ans
+			})
 		})
 	}
 
