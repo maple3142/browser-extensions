9774b5bf054b08aa350aa87b7bf791c381fef751
maple <kirby741852963@gmail.com>
2019-06-14 22:12:06 +0800

    ytdl: bump version

--- scripts/local-youtube-dl.user.js
+++ scripts/local-youtube-dl.user.js
@@ -3,7 +3,7 @@
 // @name:zh-TW   本地 YouTube 下載器
 // @name:zh-CN   本地 YouTube 下载器
 // @namespace    https://blog.maple3142.net/
-// @version      0.8.2
+// @version      0.8.3
 // @description  Get youtube raw link without external service.
 // @description:zh-TW  不需要透過第三方的服務就能下載 YouTube 影片。
 // @description:zh-CN  不需要透过第三方的服务就能下载 YouTube 影片。
