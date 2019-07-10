150e64a406d61828f36e2e387a42fdc10d586be1
maple3142 <kirby741852963@gmail.com>
2019-07-10 21:38:44 +0800

    anigadb: change bypassing method

--- scripts/anigamer-ad-bypasser.user.js
+++ scripts/anigamer-ad-bypasser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         跳過動畫瘋廣告
 // @namespace    https://blog.maple3142.net/
-// @version      0.5
+// @version      0.6
 // @description  RT
 // @author       maple3142
 // @match        https://ani.gamer.com.tw/animeVideo.php?sn=*
@@ -14,20 +14,27 @@
 
 ;(function() {
 	'use strict'
-	const sn = animefun.videoSn
-	const device = animefun.getdeviceid()
-	const s = getAd()
-	const sleep = ms => new Promise(res => setTimeout(res, ms))
-	const startad = () => xf.get('/ajax/videoCastcishu.php', { qs: { sn, s } }).text()
-	const endad = () => xf.get('/ajax/videoCastcishu.php', { qs: { sn, s, ad: 'end' } }).text()
-	const getm3u8 = () => xf.get('/ajax/m3u8.php', { qs: { sn, device } }).json()
-
-	const skipad = () =>
-		startad()
-			.then(() => sleep(3000))
-			.then(endad)
-			.then(getm3u8)
-			.then(console.log)
-			.then(() => location.reload())
-	getm3u8().then(r => (r.src ? null : skipad()))
+	const css = document.createElement('style')
+	css.textContent = `.vast-blocker{background:black;}.vast-blocker::before{content:"約8秒後會自動跳過廣告，失敗就重新整理";color:white;font-size:70px;}`
+	document.body.appendChild(css)
+	let tryCount = 0
+	function tryUntilAniVideoIsOk() {
+		tryCount++
+		if (typeof ani_video === 'undefined' || typeof ani_video.on === 'undefined') {
+			if (tryCount > 3) {
+				alert('跳過動畫瘋廣告腳本需要動畫瘋工具箱才能運作，請安裝')
+				window.open(
+					'https://greasyfork.org/zh-TW/scripts/39136-%E5%8B%95%E7%95%AB%E7%98%8B%E5%B7%A5%E5%85%B7%E7%AE%B1',
+					'_blank'
+				)
+			} else {
+				setTimeout(tryUntilAniVideoIsOk, 1000)
+			}
+			return
+		}
+		ani_video.on('vast.adStart', () => {
+			setTimeout(() => ani_video.trigger('vast.adSkip'), 8000)
+		})
+	}
+	tryUntilAniVideoIsOk()
 })()
