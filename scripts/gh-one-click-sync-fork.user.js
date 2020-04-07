4321e080322158943d57a1ba4fb21fa1819e49e4
maple3142 <kirby741852963@gmail.com>
2020-04-07 23:33:27 +0800

    ghocsf: update branchbtn id

--- scripts/gh-one-click-sync-fork.user.js
+++ scripts/gh-one-click-sync-fork.user.js
@@ -12,7 +12,7 @@
 // @compatible   chrome >=55
 // ==/UserScript==
 
-;(function() {
+;(function () {
 	'use strict'
 	const $ = s => document.querySelector(s)
 	const $$ = s => [...document.querySelectorAll(s)]
@@ -20,12 +20,17 @@
 	const oauthurl = // eslint-disable-next-line max-len
 		'https://github.com/login/oauth/authorize?client_id=5c0954a832a0f2bb68f2&scope=repo&redirect_uri=https://github.com/'
 	// eslint-disable-next-line max-len
-	const confirmmsg = 'Are you sure?\nAll the changes you have made will be DELETED!\nIT CANNOT BE RECOVERED!'
+	const confirmmsg =
+		'Are you sure?\nAll the changes you have made will be DELETED!\nIT CANNOT BE RECOVERED!'
 	const search = new URL(location.href).searchParams
 	if (search.has('code')) {
 		const code = search.get('code')
 		xf.post('https://github.com/login/oauth/access_token', {
-			json: { client_id: '5c0954a832a0f2bb68f2', client_secret: '799a5a49ef0ebde906f8bedc2e3327a99cd0d92a', code }
+			json: {
+				client_id: '5c0954a832a0f2bb68f2',
+				client_secret: '799a5a49ef0ebde906f8bedc2e3327a99cd0d92a',
+				code
+			}
 		}).text(str => {
 			const search = new URLSearchParams(str)
 			GM_setValue('token', search.get('access_token'))
@@ -41,21 +46,15 @@
 
 		if (!isUserMatch || !isFork) return
 		$$('.one-click-sync-fork').forEach(btn => btn.remove())
-		const repo = location.pathname
-			.split('/')
-			.slice(1, 3)
-			.join('/')
-		const upstream = $('.fork-flag a')
-			.getAttribute('href')
-			.slice(1)
+		const repo = location.pathname.split('/').slice(1, 3).join('/')
+		const upstream = $('.fork-flag a').getAttribute('href').slice(1)
 		const fnav = $('.file-navigation')
-		const branchbtn = $('.file-navigation>.select-menu')
+		const branchbtn = $('#branch-select-menu')
+		console.log(branchbtn)
 		if (!branchbtn) return
 		const branch =
-			fnav
-				.querySelector('a')
-				.href.split('/')
-				.pop() || branchbtn.querySelector('summary>span').textContent
+			fnav.querySelector('a').href.split('/').pop() ||
+			branchbtn.querySelector('summary>span').textContent
 		const el = document.createElement('div')
 		el.classList.add('one-click-sync-fork')
 		el.classList.add('btn')
@@ -69,7 +68,9 @@
 				return
 			}
 			if (!confirm(confirmmsg)) return
-			const sha = await gh.get(`/repos/${upstream}/branches/${branch}`).json(b => b.commit.sha)
+			const sha = await gh
+				.get(`/repos/${upstream}/branches/${branch}`)
+				.json(b => b.commit.sha)
 			await gh
 				.patch(`/repos/${repo}/git/refs/heads/${branch}`, {
 					headers: { Authorization: `token ${token}` },
