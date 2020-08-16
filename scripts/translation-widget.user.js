085585df00b847b48e196ff2083d8f08df7ca58c
maple3142 <kirby741852963@gmail.com>
2020-08-16 16:20:37 +0800

    tw: add tts support

--- scripts/translation-widget.user.js
+++ scripts/translation-widget.user.js
@@ -1,17 +1,111 @@
 // ==UserScript==
 // @name         翻譯小工具
 // @namespace    https://blog.maple3142.net/
-// @version      1.3
+// @version      1.4
 // @description  選字時會出現小懸浮窗以方便翻譯
 // @author       maple3142
 // @include      *
 // @connect      translate.google.com
 // @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 // 此腳本的各版本作者為: 田雨菲->xinggsf->maple3142
 
 'use strict'
+
+const generateGoogleTTSLink = (() => {
+	// Reference: https://stackoverflow.com/a/40078546/6885801
+	function shiftLeftOrRightThenSumOrXor(num, opArray) {
+		return opArray.reduce((acc, opString) => {
+			var op1 = opString[1] //	'+' | '-' ~ SUM | XOR
+			var op2 = opString[0] //	'+' | '^' ~ SLL | SRL
+			var xd = opString[2] //	[0-9a-f]
+
+			var shiftAmount = hexCharAsNumber(xd)
+			var mask = op1 == '+' ? acc >>> shiftAmount : acc << shiftAmount
+			return op2 == '+' ? (acc + mask) & 0xffffffff : acc ^ mask
+		}, num)
+	}
+
+	function hexCharAsNumber(xd) {
+		return xd >= 'a' ? xd.charCodeAt(0) - 87 : Number(xd)
+	}
+
+	function transformQuery(query) {
+		for (var e = [], f = 0, g = 0; g < query.length; g++) {
+			var l = query.charCodeAt(g)
+			if (l < 128) {
+				e[f++] = l //	0{l[6-0]}
+			} else if (l < 2048) {
+				e[f++] = (l >> 6) | 0xc0 //	110{l[10-6]}
+				e[f++] = (l & 0x3f) | 0x80 //	10{l[5-0]}
+			} else if (
+				0xd800 == (l & 0xfc00) &&
+				g + 1 < query.length &&
+				0xdc00 == (query.charCodeAt(g + 1) & 0xfc00)
+			) {
+				//	that's pretty rare... (avoid ovf?)
+				l =
+					(1 << 16) +
+					((l & 0x03ff) << 10) +
+					(query.charCodeAt(++g) & 0x03ff)
+				e[f++] = (l >> 18) | 0xf0 //	111100{l[9-8*]}
+				e[f++] = ((l >> 12) & 0x3f) | 0x80 //	10{l[7*-2]}
+				e[f++] = (l & 0x3f) | 0x80 //	10{(l+1)[5-0]}
+			} else {
+				e[f++] = (l >> 12) | 0xe0 //	1110{l[15-12]}
+				e[f++] = ((l >> 6) & 0x3f) | 0x80 //	10{l[11-6]}
+				e[f++] = (l & 0x3f) | 0x80 //	10{l[5-0]}
+			}
+		}
+		return e
+	}
+
+	function normalizeHash(encondindRound2) {
+		if (encondindRound2 < 0) {
+			encondindRound2 = (encondindRound2 & 0x7fffffff) + 0x80000000
+		}
+		return encondindRound2 % 1e6
+	}
+
+	function calcHash(query, windowTkk) {
+		//	STEP 1: spread the the query char codes on a byte-array, 1-3 bytes per char
+		var bytesArray = transformQuery(query)
+
+		//	STEP 2: starting with TKK index, add the array from last step one-by-one, and do 2 rounds of shift+add/xor
+		var d = windowTkk.split('.')
+		var tkkIndex = Number(d[0]) || 0
+		var tkkKey = Number(d[1]) || 0
+
+		var encondingRound1 = bytesArray.reduce((acc, current) => {
+			acc += current
+			return shiftLeftOrRightThenSumOrXor(acc, ['+-a', '^+6'])
+		}, tkkIndex)
+
+		//	STEP 3: apply 3 rounds of shift+add/xor and XOR with they TKK key
+		var encondingRound2 =
+			shiftLeftOrRightThenSumOrXor(encondingRound1, [
+				'+-3',
+				'^+b',
+				'+-f'
+			]) ^ tkkKey
+
+		//	STEP 4: Normalize to 2s complement & format
+		var normalizedResult = normalizeHash(encondingRound2)
+
+		return normalizedResult.toString() + '.' + (normalizedResult ^ tkkIndex)
+	}
+	return function generateGoogleTTSLink(q, tl, tkk) {
+		var tk = calcHash(q, tkk)
+		return `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(
+			q
+		)}&tl=${tl}&total=1&idx=0&textlen=${
+			q.length
+		}&tk=${tk}&client=webapp&prev=input`
+	}
+})()
 const googleUrl =
 	'https://translate.google.com/translate_a/single?client=gtx&dt=t&dt=bd&dt=rm&dj=1&source=input&hl=en&sl=auto'
 const reHZ = /^[\u4E00-\u9FA5\uFF00-\uFF20\u3000-\u301C]$/
@@ -197,10 +291,30 @@ function showTranslate(text, dest, originaldest = dest) {
 			}|${dest}|${encodeURIComponent(text)}`
 			a2.target = '_blank'
 			const linksWrapper = document.createElement('div')
+			const a3 = document.createElement('a')
+			a3.textContent = '播放語音'
+			a3.href = 'javascript:void(0)'
+			a3.onclick = async () => {
+				const link = generateGoogleTTSLink(
+					text,
+					res.src,
+					await getTKK()
+				)
+				const blob = await gmGetAsBlob(link)
+				const url = URL.createObjectURL(blob)
+				const aud = new Audio(url)
+				aud.play()
+				aud.onended = () => {
+					URL.revokeObjectURL(url)
+				}
+			}
+
 			linksWrapper.appendChild(a1)
 			if (originaldest != 'en')
 				linksWrapper.appendChild(document.createTextNode(' '))
 			linksWrapper.appendChild(a2)
+			linksWrapper.appendChild(document.createTextNode(' '))
+			linksWrapper.appendChild(a3)
 			translateTip.appendElement(linksWrapper)
 
 			translateTip.show()
@@ -226,6 +340,43 @@ function translate(text, dest) {
 		GM_xmlhttpRequest({
 			method: 'GET',
 			responseType: 'json',
+			url: url,
+			onload: r => res(r.response),
+			onerror: err => rej(err)
+		})
+	})
+}
+
+function getTKK(forceRefresh = false) {
+	return new Promise((res, rej) => {
+		let tkk = GM_getValue('tkk')
+		if (!forceRefresh && tkk) {
+			res(tkk)
+		}
+		GM_xmlhttpRequest({
+			method: 'GET',
+			responseType: 'text',
+			url: 'https://translate.google.com/',
+			onload: r => {
+				const html = r.response
+				const idx = html.indexOf('tkk')
+				const tkk = html.slice(idx + 5, idx + 5 + 16)
+				GM_setValue('tkk', tkk)
+				res(tkk)
+			},
+			onerror: err => {
+				if (!forceRefresh) rej(err)
+				else res(getTKK(true))
+			}
+		})
+	})
+}
+
+function gmGetAsBlob(url) {
+	return new Promise((res, rej) => {
+		GM_xmlhttpRequest({
+			method: 'GET',
+			responseType: 'blob',
 			url: url,
 			onload: r => res(r.response),
 			onerror: err => rej(err)
