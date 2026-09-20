e8f6279ab2dba94a9fc7b0c908093cad6a721512
maple3142 <kirby741852963@gmail.com>
2026-09-20 11:47:11 +0800

    termpttcc: update to work on new version

--- scripts/termpttcc-autologin.user.js
+++ scripts/termpttcc-autologin.user.js
@@ -1,92 +1,101 @@
 // ==UserScript==
 // @name         term.ptt.cc 自動登入
 // @namespace    https://blog.maple3142.net/
-// @version      0.3.0
+// @version      0.4.0
 // @description  自動登入 term.ptt.cc + 自動跳過一些畫面
 // @author       maple3142
 // @match        https://term.ptt.cc/
 // @run-at       document-start
 // @grant        unsafeWindow
 // @grant        window.close
-// @compatible   firefox >=52
-// @compatible   chrome >=55
 // @license      MIT
 // ==/UserScript==
 
-const ID = ''
-const PASS = ''
 const skiplist = [
-	{ re: /歡迎您再度拜訪，上次您是從.*連往本站。/, input: '\n' }, //跳過啟動頁 1
+	{ re: /歡迎您再度拜訪，上次您是從.*連往本站。.*請按任意鍵繼續/, input: '\n' }, //跳過啟動頁 1
 	{ re: /單一小時上線人次.*單日上線人次/, input: 'q' }, // 跳過啟動頁 2
 	{ re: /您想刪除其他重複登入的連線嗎？\[Y\/n\]/, input: 'y\n' }, // 自動刪除重複登入
 	{ re: /上方為使用者心情點播留言區，不代表本站立場/, input: 'f\n' } // 自動進入我的最愛(第一次)
 ]
-const closeOnQuit = true
-const closeWarning = true
-;(function() {
+const closeOnQuit = true // 手動從 ptt 選擇離開 or 斷線時自動關閉頁面
+;(() => {
 	'use strict'
-	// event
-	const E = {
-		callbacks: {},
-		on(evt, cb) {
-			if (!Array.isArray(this.callbacks[evt])) this.callbacks[evt] = []
-			this.callbacks[evt].push(cb)
+	const onApp = app => {
+		// helpers
+		const insertText = str => app.send(str.replace('\n', '\r'))
+
+		function getScreenText(app) {
+			const { buf } = app
+			return Array.from({ length: buf.rows }, (_, row) => buf.getRowText(row, 0, buf.cols)).join('')
+		}
+
+		const KEY = 'ptt-credentials'
+		const loginAssist = app.getPlugin('login_assist')
+
+		// hook loginAssist.handleLogin to store credentials
+		const origLogin = loginAssist.handleLogin
+		loginAssist.handleLogin = credentials => {
+			console.log(credentials)
+			const { username, password } = credentials
+			if (username && password) {
+				localStorage.setItem(KEY, JSON.stringify({ username, password }))
+			}
+			return origLogin(credentials)
+		}
+
+		// the terminal will detect login automatically, try it if login credentials are stored and not yet attempted
+		let autoLoginAttempted = false
+		app.on('term:login-prompt', () => {
+			if (autoLoginAttempted) return
+
+			if (!(KEY in localStorage)) return
+			const credentials = JSON.parse(localStorage.getItem(KEY))
+			if (!credentials?.username || !credentials?.password) return
+
+			autoLoginAttempted = true
+			loginAssist.handleLogin(credentials)
+		})
+		// to skip some screens
+		app.on('term:screen-update', () => {
+			if (skiplist.every(x => x.executed)) return // skip regex matching if all executed
+			const t = getScreenText(app)
+			if (/請輸入代號，或以 guest 參觀，或以 new 註冊:.*(請重新輸入。|密碼不對或無此帳號)/.test(t)) {
+				// stored password is probably wrong, clear it and refresh
+				localStorage.removeItem(KEY)
+				location.reload()
+			}
+			const rule = skiplist.find(rule => !rule.executed && rule.re.test(t))
+			if (!rule) return
+			console.log('executeed', rule, t)
+			rule.executed = true
+			insertText(rule.input)
+		})
+		// close the page when disconnected
+		if (closeOnQuit) app.on('term:disconnect', close)
+
+		// add a notice about storing credentials in localStorage
+		const seen = new WeakSet()
+		const addNotice = () => {
+			document.querySelectorAll('p.LoginModal__Hint').forEach(hint => {
+				if (seen.has(hint)) return
+				seen.add(hint)
+				hint.append(
+					document.createElement('br'),
+					'term.ptt.cc 自動登入腳本會將帳密儲存於 localStorage 本機儲存空間，以供下次自動登入。密碼未加密，請勿在共用電腦使用。'
+				)
+			})
+		}
+		new MutationObserver(addNotice).observe(document.body, {
+			childList: true,
+			subtree: true
+		})
+	}
+	let app = null
+	Object.defineProperty(unsafeWindow, 'app', {
+		set(x) {
+			if (!app) onApp(x)
+			app = x
 		},
-		emit(evt, ...args) {
-			if (!Array.isArray(this.callbacks[evt])) return
-			this.callbacks[evt].forEach(cb => cb(...args))
-		},
-		off(evt, cb) {
-			if (!cb) this.callbacks[evt] = []
-			else this.callbacks = this.callbacks.filter(x => x !== cb)
-		}
-	}
-
-	// helpers
-	const insertText = (() => {
-		let t = document.querySelector('#t')
-		return str => {
-			if (!t) t = document.querySelector('#t')
-			const e = new CustomEvent('paste')
-			e.clipboardData = { getData: () => str }
-			t.dispatchEvent(e)
-		}
-	})()
-	function hook(obj, key, cb) {
-		const fn = obj[key].bind(obj)
-		obj[key] = function(...args) {
-			fn.apply(this, args)
-			cb.apply(this, args)
-		}
-	}
-
-	// console hook
-	hook(unsafeWindow.console, 'info', t => {
-		if (typeof t !== 'string' || !/pttchrome (\w+)/.test(t)) return
-		const evt = /pttchrome (\w+)/.exec(t.trim())[1]
-		E.emit(evt.replace(/^on/, '').toLowerCase())
+		get: () => app
 	})
-	hook(unsafeWindow.console, 'log', t => {
-		if (t === 'view update') E.emit('update')
-	})
-
-	// main
-	E.on('connect', () => insertText(ID + '\n' + PASS + '\n'))
-	E.on('update', () => {
-		const t = Array.prototype.map.call(document.querySelectorAll('span[type=bbsRow]'), x => x.textContent).join('')
-		const ar = skiplist.filter(x => x.re.test(t))
-		for (let i = 0; i < ar.length; i++) {
-			if (ar[i].executed) continue
-			insertText(ar[i].input)
-			ar[i].executed = true
-		}
-		if (skiplist.filter(x => x.executed).length === skiplist.length) E.off('update')
-	})
-	if (closeOnQuit) E.on('close', close)
-	if (!closeWarning) {
-		unsafeWindow.addEventListener = (fn => (...args) => {
-			if (args[0] === 'beforeunload') return
-			fn(...args)
-		})(unsafeWindow.addEventListener.bind(unsafeWindow))
-	}
 })()
